39b8e3350499408bca6ba7770d885fabb246a084
Franck W <franck.rj.w@gmail.com>
2019-04-05 08:55:48 +0200

    Ajout de la fonction de base a injecter.

---
+++ alloc_null.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include "alloc_null.h"
+
+void	*malloc_null(size_t size)
+{
+	static time_t seed = 0;
+
+	if (seed == 0)
+	{
+		seed = (CUSTOM_SEED == 0 ? time(NULL) : CUSTOM_SEED);
+		srand(seed);
+		dprintf(STDERR_FILENO, "Seed : %u\n", (unsigned)seed);
+	}
+	if (rand() % 100 < PERCENT_CHANCE_FAIL)
+		return (NULL);
+	else
+		return (malloc(size));
+}
---
+++ alloc_null.h
@@ -0,0 +1,11 @@
+#ifndef ALLOC_NULL_H
+# define ALLOC_NULL_H
+
+# include <stdlib.h>
+
+# define PERCENT_CHANCE_FAIL	30
+# define CUSTOM_SEED			0
+
+void	*malloc_null(size_t size);
+
+#endif
